f83d6d57687a95499bda17b533ebb5e545b5be20
KELLI KARTHIK <73167875+karthik160614@users.noreply.github.com>
2026-03-03 17:54:37 +0530

    Add files via upload
    New V

---
+++ componentsM/rate.js
@@ -0,0 +1,7 @@
+const rate1=document.getElementsByClassName("proR");
+const rate=document.getElementsByClassName("rate");
+const sub=document.getElementsByClassName("sub");
+sub.onclick()= function(){
+    let v=rate1.value;
+    rate.value=v`;
+}
